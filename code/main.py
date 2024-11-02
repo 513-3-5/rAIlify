be1a799c2c4e7c0d92bb4c1f6dcf1986568c34b9
noahgrun <noah.grun@bluewin.ch>
2024-11-02 12:54:28 +0100

    Add a .tif to .pdf converter to work with pdf's in later steps

--- code/main.py
+++ code/main.py
@@ -1,11 +1,14 @@
 import sys
 import os
 from PyPDF2 import PdfReader
-
+from PIL import Image
 
 
 def is_pdf(file_path):
     return file_path.lower().endswith(".pdf")
+
+def is_tiff(file_path):
+    return file_path.lower().endswith((".tif", ".tiff"))
 
 def count_pages_in_pdf(file_path):
     try:
@@ -16,21 +19,37 @@ def count_pages_in_pdf(file_path):
         print(f"Error reading from file: {e}")
         return None
     
-def is_valid_pdf(pdf_path):
-    if not os.path.isfile(pdf_path):
+def convert_tiff_to_pdf(file_path):
+    output_pdf_path = file_path.rsplit(".", 1)[0] + ".pdf"
+    try:
+        # Öffne das TIFF-Bild
+        with Image.open(file_path) as img:
+            img = img.convert("RGB")
+            img.save(output_pdf_path, "PDF")
+            print(f"Info: File successfully saved as PDF: {output_pdf_path}")
+            return output_pdf_path
+    except Exception as e:
+        print(f"Error converting the .tif file to a PDF: {e}")
+        sys.exit(1)    
+    
+def is_valid_pdf(file_path):
+    if not os.path.isfile(file_path):
         print("Error: The provided file doesn't exist.")
         return False
-
-    if not is_pdf(pdf_path):
+    
+    if is_tiff(file_path):
+        file_path = convert_tiff_to_pdf(file_path)
+        
+    if not is_pdf(file_path):
         print("Error: The provided file is not a .pdf file.")
         return False
 
-    page_count = count_pages_in_pdf(pdf_path)
+    page_count = count_pages_in_pdf(file_path)
     if page_count is None:
         print("Error: The provided .pdf file has no pages.")
         return False
-    # if page_count > 1: # TODO: Check if some files may have multiple plans or if they always have a single railsystem.
-    #     print(f"Error: The provided .pdf file has {page_count} pages instead of one.") 
+    elif page_count > 1: # TODO: Check if some files may have multiple plans or if they always have a single railsystem.
+        print(f"The provided .pdf file has {page_count} pages") 
     
 
     print("Valid pdf file")
@@ -39,16 +58,16 @@ def is_valid_pdf(pdf_path):
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("Error: Please provide a .pdf file.")
+        print("Error: Please provide a .pdf or .tif file.")
         sys.exit(1)          
 
-    # if len(sys.argv) < 2: # TODO: probably multiple pdf files possible?
-    #    print("Please only provide a single .pdf file.")
-    #    sys.exit(1)
+    if len(sys.argv) < 2:  
+        print("Please only provide a single .pdf or .tif file.")
+        sys.exit(1)
 
-    pdf_path = sys.argv[1]
+    file_path = sys.argv[1]
 
-    if not is_valid_pdf(pdf_path):
+    if not is_valid_pdf(file_path):
         sys.exit(1)
 
 
