b9c94846dc3f0372e93b7253e2cc681e76ee5938
Zora Fuchs <bailiff.dolmens.0i@icloud.com>
2024-11-02 10:55:35 +0100

    Add ruby script for visualizing a basic data structure

---
+++ code/visualization/visualizer.rb
@@ -0,0 +1,43 @@
+require "json"
+
+file = File.read('example.json')
+data = JSON.parse(file)
+
+nodes = data.filter do |node|
+  node["type"] == "node"
+end.sort_by do |node|
+  [node["originX"], node["originY"]]
+end
+
+edges = data.filter do |node|
+  node["type"] == "edge"
+end
+
+visualizing_array = [[nodes[0]]]
+nodes.shift
+
+loop do
+  next_level = []
+  visualizing_array.last.each do |node|
+    next_level += edges.find_all { |edge| edge["parents"].include?(node["uuid"]) }
+    edges.each { |edge| edge["parents"].delete_if { |parent| parent == node["uuid"] } }
+  end
+  visualizing_array << next_level.uniq
+  new_level = []
+  visualizing_array.last.each do |edge|
+    new_level += edge["parents"].map { |parent| nodes.find { |node| node["uuid"] == parent } }
+    nodes.delete_if { |node| edge["parents"].find { |parent| node["uuid"] == parent } }
+    edges.each { |original_edge| edge["parents"] = [] if original_edge["uuid"] == edge["uuid"]}
+  end
+  visualizing_array << new_level.uniq
+  break if nodes.empty?
+end
+
+visualizing_array.map do |row|
+  row.map do |column|
+    print "|" if column["element"] == "Gleisabschnitt"
+    print "*" if column["element"] == "Radzähler"
+    print "^" if column["element"] == "Weiche"
+  end
+  print "\n"
+end
