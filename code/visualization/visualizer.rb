586f8540181783f046447272d5575bc80e0941e1
Zora Fuchs <bailiff.dolmens.0i@icloud.com>
2024-11-03 08:49:13 +0100

    Refactor and align track graph independently of objects

--- code/visualization/visualizer.rb
+++ code/visualization/visualizer.rb
@@ -7,28 +7,29 @@ ARGV.each do |filename|
   data = JSON.parse(file)
 
 
-  nodes = data.filter { |data_obj| data_obj["type"] == "node" }.sort_by { |node| [node["originX"], node["originY"]] }
-  objects = data.filter { |data_obj| data_obj["type"] == "object" }.sort_by { |object| [object["originX"], object["originY"]] }
-  edges = data.filter { |data_obj| data_obj["type"] == "edge" }
+  nodes = data.filter { |data_obj| data_obj['type'] == 'node' }.sort_by { |node| [node['originX'], node['originY']] }
+  objects = data.filter { |data_obj| data_obj['type'] == 'object' }.sort_by { |object| [object['originX'], object['originY']] }
+  edges = data.filter { |data_obj| data_obj['type'] == 'edge' }
 
   graph = RGL::AdjacencyGraph.new
-  nodes.each { |node| graph.add_vertex(node) }
+
+  nodes.each do |node|
+    graph.add_vertex(node['uuid'])
+    graph.set_vertex_options(node['uuid'], label: "#{node['element']}\n#{node['name']}", group: 'track')
+  end
 
   edges.each do |edge|
-    u = graph.find { |vertex| vertex['uuid'] == edge['parents'][0] }
-    v = graph.find { |vertex| vertex['uuid'] == edge['parents'][1] }
-    graph.add_edge(u, v)
-    graph.set_edge_options(u, v, label: "#{edge['name']}")
+    graph.add_edge(edge['parents'][0], edge['parents'][1])
+    graph.set_edge_options(edge['parents'][0], edge['parents'][1], label: "#{edge['name']}")
   end
 
   objects.each do |object|
     unless object['parents'].empty?
-      v = graph.each_vertex.find { |vertex| vertex['uuid'] == object['parents'][0] }
-      graph.add_edge(object, v)
-      graph.set_edge_options(object, v, style: 'dotted' )
+      graph.add_edge(object['uuid'], object['parents'][0])
+      graph.set_vertex_options(object['uuid'], label: "#{object['element']}\n#{object['name']}")
+      graph.set_edge_options(object['uuid'], object['parents'][0], style: 'dotted')
     end
   end
 
-  graph.each_vertex { |vertex| graph.set_vertex_options(vertex, label: "#{vertex['element']}\n#{vertex['name']}") }
   graph.write_to_graphic_file('png', "output/#{filename.split(".").first}")
 end
