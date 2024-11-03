ff18259d1512cd6cca08f9268ffba846e05ef449
Zora Fuchs <bailiff.dolmens.0i@icloud.com>
2024-11-03 07:52:35 +0100

    Add related object to graph

--- code/visualization/visualizer.rb
+++ code/visualization/visualizer.rb
@@ -12,14 +12,22 @@ ARGV.each do |filename|
   edges = data.filter { |data_obj| data_obj["type"] == "edge" }
 
   graph = RGL::AdjacencyGraph.new
-
   nodes.each { |node| graph.add_vertex(node) }
-  edges.each { |edge|
+
+  edges.each do |edge|
     u = graph.find { |vertex| vertex['uuid'] == edge['parents'][0] }
     v = graph.find { |vertex| vertex['uuid'] == edge['parents'][1] }
     graph.add_edge(u, v)
     graph.set_edge_options(u, v, label: "#{edge['name']}")
-  }
+  end
+
+  objects.each do |object|
+    unless object['parents'].empty?
+      v = graph.each_vertex.find { |vertex| vertex['uuid'] == object['parents'][0] }
+      graph.add_edge(object, v)
+      graph.set_edge_options(object, v, style: 'dotted' )
+    end
+  end
 
   graph.each_vertex { |vertex| graph.set_vertex_options(vertex, label: "#{vertex['element']}\n#{vertex['name']}") }
   graph.write_to_graphic_file('png', "output/#{filename.split(".").first}")
